4a6b2bddd022d7671087beee4c154c79404567a5
atefeh199170 <72982087+atefeh199170@users.noreply.github.com>
2021-08-26 12:53:45 +0430

    updated input.ts and input.scss

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
-export * from './button/buttons';
-export * from './input/inputs';
+export * from './button/button';
+export * from './input/input';
 export * from './label/label'
 export * from './textarea/textareas';
